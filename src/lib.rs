b2975f75f445d3233abd05309aa3f0447d3027d5
Julian Giamblanco <giamblanco.julian@gmail.com>
2024-08-09 22:41:18 -0700

    Add joins & export

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub use anyhow::Result;
+use polars::{lazy::prelude::*, prelude::*};
 use std::path::PathBuf;
 
 #[derive(Debug, thiserror::Error)]
@@ -11,45 +12,120 @@ pub enum Error {
 #[serde(try_from = "PathBuf")]
 pub struct RealPathBuf(PathBuf);
 
+impl AsRef<std::path::Path> for RealPathBuf {
+    fn as_ref(&self) -> &std::path::Path {
+        self.0.as_ref()
+    }
+}
+
 impl TryFrom<PathBuf> for RealPathBuf {
     type Error = anyhow::Error;
 
     fn try_from(value: PathBuf) -> std::result::Result<Self, Self::Error> {
         if value.try_exists()? {
-            Ok(Self(value))
+            Ok(Self(value.canonicalize()?))
         } else {
             Err(Error::BadPath(value).into())
         }
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Eq)]
-pub struct FileSource {
-    pub path: RealPathBuf,
+#[typetag::serde(tag = "type")]
+pub trait DataSource: std::fmt::Debug {
+    // fn name(&self) -> &str;
+    fn to_lazy_frame(&self) -> anyhow::Result<LazyFrame>;
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Eq)]
-#[serde(tag = "type", rename_all = "lowercase")]
-pub enum DataSourceType {
-    Csv(FileSource),
-
-    Excel(FileSource),
+impl TryFrom<&Box<dyn DataSource>> for LazyFrame {
+    type Error = anyhow::Error;
+    fn try_from(value: &Box<dyn DataSource>) -> std::result::Result<Self, Self::Error> {
+        value.to_lazy_frame()
+    }
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Eq)]
-pub struct DataSource {
-    #[serde(flatten)]
-    r#type: DataSourceType,
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct CsvSource {
+    path: RealPathBuf,
+}
+
+#[typetag::serde(name = "csv")]
+impl DataSource for CsvSource {
+    fn to_lazy_frame(&self) -> anyhow::Result<LazyFrame> {
+        Ok(LazyCsvReader::new(&self.path)
+            .with_has_header(true)
+            .finish()?)
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum Join {
+    Inner,
+    Left,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct JoinSource {
+    left: Box<dyn DataSource>,
+    left_on: String,
+    right: Box<dyn DataSource>,
+    right_on: String,
+    how: Join,
+}
+
+#[typetag::serde(name = "join")]
+impl DataSource for JoinSource {
+    fn to_lazy_frame(&self) -> anyhow::Result<LazyFrame> {
+        let lf1 = self.left.to_lazy_frame()?;
+        let lf2 = self.right.to_lazy_frame()?;
+        Ok(lf1.join(
+            lf2,
+            [col(&self.left_on)],
+            [col(&self.right_on)],
+            JoinArgs::new(match self.how {
+                Join::Inner => JoinType::Inner,
+                Join::Left => JoinType::Left,
+            })
+            .with_coalesce(JoinCoalesce::CoalesceColumns),
+        ))
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct Exporter {
+    folder: PathBuf,
     name: String,
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Eq)]
+impl Exporter {
+    pub fn export(&self, lf: LazyFrame) -> anyhow::Result<()> {
+        std::fs::create_dir_all(&self.folder)?;
+        let mut filename = self.folder.clone();
+        filename.push(format!(
+            "{}_{}.csv",
+            self.name,
+            chrono::Local::now().naive_local().to_string()
+        ));
+        lf.sink_csv(
+            &filename,
+            CsvWriterOptions {
+                ..Default::default()
+            },
+        )?;
+        Ok(())
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub struct EtlJob {
-    sources: Vec<DataSource>,
+    source: Box<dyn DataSource>,
+    export: Exporter,
 }
 
 impl EtlJob {
     pub fn run(&self) -> Result<()> {
+        let lf: LazyFrame = (&self.source).try_into()?;
+        self.export.export(lf)?;
         Ok(())
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,34 +4,43 @@ use std::str::FromStr;
 #[test]
 fn it_works() {
     let config: &str = r#"
-    [[sources]]
-    name = "Test csv"
+
+    [source]
+    type = "join"
+    how = "left"
+    left_on = "Foo"
+    right_on = "Foo"
+
+    [source.left]
     type = "csv"
     path = "test.csv"
 
-    [[sources]]
-    name = "Test xlsx"
-    type = "excel"
-    path = "test.xlsx"
+    [source.right]
+    type = "csv"
+    path = "test2.csv"
+
+
+    [export]
+    folder = "./output"
+    name = "export"
+
     "#;
     let job: EtlJob = toml::from_str(config).unwrap();
-    assert_eq!(
-        job,
-        EtlJob {
-            sources: vec![
-                DataSource {
-                    r#type: DataSourceType::Csv(FileSource {
-                        path: RealPathBuf(PathBuf::from_str("test.csv").unwrap())
-                    }),
-                    name: "Test csv".to_owned()
-                },
-                DataSource {
-                    r#type: DataSourceType::Excel(FileSource {
-                        path: RealPathBuf(PathBuf::from_str("test.xlsx").unwrap())
-                    }),
-                    name: "Test xlsx".to_owned()
-                }
-            ]
-        }
-    );
+    // TODO: Fully implement PartialEq for the DataSource trait
+    // otherwise, figure out a different way of testing
+    // assert_eq!(
+    //     job,
+    //     EtlJob {
+    //         sources: vec![
+    //             Box::new(CsvSource {
+    //                 path: RealPathBuf(PathBuf::from_str("test.csv").unwrap()),
+    //                 name: "Test csv".to_owned()
+    //             }),
+    //             // Box::new(ExcelSource {
+    //             //     path: RealPathBuf(PathBuf::from_str("test.xlsx").unwrap()),
+    //             //     name: "Test xlsx".to_owned()
+    //             // })
+    //         ]
+    //     }
+    // );
 }
