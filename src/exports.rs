2772e320f9e6a6ff83039bedc6930800c4277ee1
Julian Giamblanco <giamblanco.julian@gmail.com>
2025-03-07 12:50:18 -0800

    feat: add json dump export, option to disable csv sink

--- src/exports.rs
+++ src/exports.rs
@@ -1,7 +1,7 @@
 //! Available methods for exporting data.
 
 use anyhow::Result;
-use polars::lazy::prelude::*;
+use polars::{io::SerWriter, lazy::prelude::*, prelude::CsvWriter};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::{
@@ -21,7 +21,9 @@ pub trait Export: Serialize + for<'a> Deserialize<'a> + JsonSchema + Debug {
 pub enum ExportItem {
     /// Export data to CSV.
     Csv(CsvExport),
-    /// Export data to JSON lines.
+    /// Export data to newline-delimited JSON.
+    NdJson(NdJsonExport),
+    /// Collect and serialize the dataframe itself to a single JSON object. You probably don't need this.
     Json(JsonExport),
 }
 
@@ -29,6 +31,7 @@ impl ExportItem {
     pub(crate) fn export(&self, lf: LazyFrame) -> Result<()> {
         match self {
             Self::Csv(export) => export.export(lf),
+            Self::NdJson(export) => export.export(lf),
             Self::Json(export) => export.export(lf),
         }
     }
@@ -44,6 +47,10 @@ pub struct CsvExport {
     /// Optional format string to append the current time to the filename -
     /// refer to <https://docs.rs/chrono/latest/chrono/format/strftime/index.html> for available format codes.
     pub date_format: Option<String>,
+    /// Whether to lazily sink data to the CSV. Defaults to true. Set to false if necessary to resolve errors.
+    /// If set to false, all data will be loaded into memory as a [`polars::prelude::DataFrame`] before being
+    /// written to disk.
+    pub sink: Option<bool>,
 }
 
 impl Export for CsvExport {
@@ -60,11 +67,54 @@ impl Export for CsvExport {
             )?
         }
         filename.write_str(".csv")?;
-        lf.sink_csv(
+        if self.sink.unwrap_or(true) {
+            lf.sink_csv(
+                self.folder.join(filename),
+                CsvWriterOptions {
+                    ..Default::default()
+                },
+                None,
+            )?;
+        } else {
+            let mut file = std::fs::File::create(self.folder.join(filename))?;
+            CsvWriter::new(&mut file)
+                .include_header(true)
+                .with_separator(b',')
+                .finish(&mut lf.collect()?)?;
+        }
+        Ok(())
+    }
+}
+
+/// Export data to newline-delimited JSON.
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+pub struct NdJsonExport {
+    /// Folder in which to create files.
+    pub folder: PathBuf,
+    /// Name of the output file, not including the file extension.
+    pub name: String,
+    /// Optional format string to append the current time to the filename -
+    /// refer to <https://docs.rs/chrono/latest/chrono/format/strftime/index.html> for available format codes.
+    pub date_format: Option<String>,
+}
+
+impl Export for NdJsonExport {
+    fn export(&self, lf: LazyFrame) -> Result<()> {
+        std::fs::create_dir_all(&self.folder)?;
+        let mut filename = String::new();
+        filename.write_str(&self.name)?;
+        if let Some(fstring) = &self.date_format {
+            filename.write_str(
+                &chrono::Local::now()
+                    .naive_local()
+                    .format(&fstring)
+                    .to_string(),
+            )?
+        }
+        filename.write_str(".jsonl")?;
+        lf.sink_json(
             self.folder.join(filename),
-            CsvWriterOptions {
-                ..Default::default()
-            },
+            JsonWriterOptions::default(),
             None,
         )?;
         Ok(())
@@ -96,12 +146,10 @@ impl Export for JsonExport {
                     .to_string(),
             )?
         }
-        filename.write_str(".jsonl")?;
-        lf.sink_json(
-            self.folder.join(filename),
-            JsonWriterOptions::default(),
-            None,
-        )?;
+        filename.write_str(".json")?;
+        let file = std::fs::File::create(self.folder.join(filename))?;
+        let df = lf.collect()?;
+        serde_json::to_writer(file, &df)?;
         Ok(())
     }
 }
