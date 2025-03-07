6005121fbdd9edfb49a8f0625e037c6b162fd545
Julian Giamblanco <giamblanco.julian@gmail.com>
2025-03-07 12:52:47 -0800

    feat: add option to dump inferred schema

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! CLI to run commands relating to `retl` configuration files.
 //! The `dump-schema` subcommand can be used to dump a JSON schema to disk using [`schemars`].
 
-use std::path::PathBuf;
+use std::{io::Write, path::PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
@@ -24,13 +24,23 @@ enum Cli {
 struct RunArgs {
     /// Path to the configuration file to run.
     config: PathBuf,
+    #[arg(long)]
+    dump_schema: Option<PathBuf>,
 }
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     match Cli::parse() {
         Cli::Run(args) => Config::from_path(&args.config.canonicalize()?, |config| {
             debug!("Running parsed config: {:?}", config);
-            config.run()
+            if let Some(path) = &args.dump_schema {
+                let schema = config.load()?.collect_schema()?.as_ref().clone();
+                let mut writer = std::fs::File::create(path)?;
+                writer.write(toml::to_string_pretty(&schema)?.as_bytes())?;
+                writer.flush()?;
+                Ok(())
+            } else {
+                config.run()
+            }
         }),
         Cli::DumpSchema { path } => {
             let schema = schema_for!(Config);
